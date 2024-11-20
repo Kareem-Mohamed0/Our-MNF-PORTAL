85b62425fea80d928fe52e55b6c5770bdf8139dd
Kareem-Mohamed0 <KAREEM.mohamed3321@el-eng.menofia.edu.eg>
2024-11-20 14:45:36 +0200

    Add project files.

---
+++ MNF-PORTAL-API/Program.cs
@@ -0,0 +1,77 @@
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MNF_PORTAL_Core.Entities;
+using MNF_PORTAL_Infrastructure.Data;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace MNF_PORTAL_API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            // Configure Identity
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+            var app = builder.Build();
+
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            /*=================== Mahmoud =====================*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+            ////////////////////////////////////////
+            /*===================== kareem ==========================*/
+
+
+            //////////////////////////////////////////////////////
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
